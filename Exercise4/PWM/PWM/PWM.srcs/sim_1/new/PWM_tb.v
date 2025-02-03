e473368d80d841b23e9671ce488b50149198b82e
Dawson Chitwood <dchitwood33@gmail.com>
2025-02-03 17:59:25 -0500

    got burst mode for 8 subdivisions work

--- Exercise4/PWM/PWM/PWM.srcs/sim_1/new/PWM_tb.v
+++ Exercise4/PWM/PWM/PWM.srcs/sim_1/new/PWM_tb.v
@@ -20,7 +20,21 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module PWM_tb(
+module PWM_tb();
+reg [15:0] period_tb = 6000;
+reg [7:0] duty_tb = 50;
+reg burstmode_tb = 1;
+reg bursttype_tb = 1;
+reg sysclk_tb = 0;
+reg reset_tb = 0;
+wire pwm_tb;
 
-    );
+
+PWM p(period_tb,duty_tb,burstmode_tb,bursttype_tb,sysclk_tb,reset_tb,pwm_tb);
+
+always begin
+    #1 sysclk_tb <= 1;
+    #1 sysclk_tb <= 0;
+end
+
 endmodule
--- Exercise4/PWM/PWM/PWM.srcs/sources_1/new/PWM.v
+++ Exercise4/PWM/PWM/PWM.srcs/sources_1/new/PWM.v
@@ -20,21 +20,87 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module PWM( input SysClk,
+module PWM (input [15:0] Period, input [7:0] Duty,input BurstMode,input BurstType, input SysClk,
             input Reset, 
-            input [15:0] Period,
-            input [7:0] Duty,
-            input BurstMode,
-            input BurstType,
-            output PWM
+            output pwm
     );
     
+    reg [15:0] counter = 0;
+    reg [15:0] subcounter = 0;
+    reg [15:0] substop = 0;
+    reg [15:0] subperiod = 0;
+    reg [15:0] stop = 0;
+    reg out = 0;
     
-    reg [16:0] Count;
+    assign pwm = out; 
     
-    reg [16:0] POSINTERVAL;
-    reg [16:0] NEGINTERVAL;
+   
+    initial begin
+         stop = Period*Duty/100;
+    end
     
+    initial begin
+    if (BurstMode) begin
+        subcounter <= 0;
+        if (BurstType) begin
+            subperiod <= stop/16;   
+        end else
+             subperiod = stop/8;
+        substop = subperiod/2;
+    end
+    end
 
+
+  
+   
+    always @ (posedge SysClk) begin
     
+        if (Reset) begin
+            counter <= 0;
+            out <= 0;
+        end
+     if(~BurstMode) begin   
+        if (counter < stop) begin
+            counter <= counter + 1;
+            out <= 1;
+        end else if (counter < Period) begin
+            counter <= counter + 1;
+            out <= 0;
+        end else 
+            counter <= 1;
+    end else 
+         if (counter < Period) begin
+                if (counter < stop) begin
+                    if (subcounter < subperiod) begin
+                        if (subcounter < substop) begin
+                            counter <= counter + 1;
+                            subcounter <= subcounter + 1;
+                            out <= 1;
+                        end else begin          
+                            counter <= counter + 1;
+                            subcounter <= subcounter + 1;
+                            out <= 0;
+                        end                       //end (subcounter < substop)
+                     end else begin
+                            subcounter <= 0;
+                            counter <= counter + 1;
+                            out <= 0;
+                     end                         //end (subcounter < subperiod)
+                 end else begin
+                        subcounter <= 0;
+                        counter <= counter + 1;
+                        out <= 0;
+                 end                          // end (counter < stop)
+            end else begin
+                subcounter <= 0;
+                counter <= 0;
+                out <= 0;
+            end                             // end (counter < Period)
+                                 
+        
+        
+            
+    end
+           
+
 endmodule
