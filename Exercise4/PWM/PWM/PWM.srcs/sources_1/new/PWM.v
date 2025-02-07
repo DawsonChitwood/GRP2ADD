402c82e750b88cdc0162018b420d59f7f67ca26e
Dawson Chitwood <dchitwood33@gmail.com>
2025-02-06 22:59:17 -0500

    Commented code and added credits to github

--- Exercise4/PWM/PWM/PWM.srcs/sources_1/new/PWM.v
+++ Exercise4/PWM/PWM/PWM.srcs/sources_1/new/PWM.v
@@ -24,20 +24,20 @@ module PWM (input [15:0] Period, input [7:0] Duty,input BurstMode,input BurstTyp
             input Reset, 
             output pwm);
             
- reg [15:0] Counter;
- reg [15:0] TransitionTime;
- reg [15:0] BurstCounter;
- reg [15:0] BurstPeriod;
- reg [15:0] BurstTransitionTime;
- reg Out;
- reg [2:0] CurrentState = START, NextState = START;
+ reg [15:0] Counter;          //Variable to keep track of when the period has ended for the signal
+ reg [15:0] TransitionTime;   //Variable to keep track of when the signal needs to transition from high to low
+ reg [15:0] BurstCounter;     //Variable to keep track of when the burst division period needs to end
+ reg [15:0] BurstPeriod;      //The period of an individual burst within the signal's high cycle
+ reg [15:0] BurstTransitionTime;  //Variable to keep track of when the signal needs to transition from high to low within a burst division
+ reg Out;                          //Variable used to indirectly assign values to the pwm output
+ reg [2:0] CurrentState = START, NextState = START;   //State variables
  
- parameter START = 0, ON = 1, OFF = 2, BURSTON = 3, BURSTOFF = 4, BURSTSWITCH = 5, SWITCH = 6;
+ parameter START = 0, ON = 1, OFF = 2, BURSTON = 3, BURSTOFF = 4, BURSTSWITCH = 5, SWITCH = 6;  //State parameters
  
- assign pwm = Out;
+ assign pwm = Out;  //Indirect assignment of output value
  
  
- //Variable Initialization
+ //Variable Initialization ~ mainly for calculating the appropriate burst-relevant values based on BurstType
  initial begin
     TransitionTime = (Period * Duty)/100;
     if(BurstType) begin
@@ -60,31 +60,32 @@ module PWM (input [15:0] Period, input [7:0] Duty,input BurstMode,input BurstTyp
  //Next State Logic
  always @ (CurrentState,Counter) begin
     case (CurrentState) 
-        START:  
+        START:                                         //FSM starts off at this stage and moves on based on BurstMode
                 if(BurstMode) NextState = BURSTON;
                 else NextState = ON;
-        ON:     
+        ON:                                           //The PWM-High state for non-burstmode operation
                 if(Counter >= Period - 1) NextState = SWITCH;
                 else if(Counter >= TransitionTime - 1) NextState = OFF;
                 else NextState = ON;
-        OFF:    
+        OFF:                                          //The common off state of the FSM
                 if(Counter >= Period - 1) NextState = SWITCH;
                 else NextState = OFF;
-        BURSTON:
+        BURSTON:                                     //The on state for when burst mode is activated
                 if(Counter >= Period - 1) NextState = SWITCH;
                 else if(Counter >= TransitionTime - 1) NextState = OFF;
                 else if (BurstCounter >= BurstTransitionTime - 1) NextState = BURSTOFF;
                 else NextState = BURSTON;
-        BURSTOFF:
+        BURSTOFF:                                   //The off state for when burst mode is activated
                 if(Counter >= Period - 1 ) NextState = SWITCH;
                 else if(Counter >= TransitionTime - 1) NextState = OFF;
                 else if(BurstCounter >= BurstPeriod - 1) NextState = BURSTSWITCH;
                 else NextState = BURSTOFF;
-        BURSTSWITCH:
+        BURSTSWITCH:                                //The state used to transition back to BURSTON from BURSTOFF while the overall 
+                                                    //transition threshold has not yet been reached
                 if(Counter >= Period - 1 ) NextState = SWITCH;
                 else if(Counter >= TransitionTime - 1) NextState = OFF;
                 else NextState = BURSTON;
-        SWITCH: 
+        SWITCH:                                     //The state used to transition from OFF to the next mode (based on BurstMode)
                 if(BurstMode) NextState = BURSTON;
                 else NextState = ON;
         default:
@@ -94,7 +95,8 @@ module PWM (input [15:0] Period, input [7:0] Duty,input BurstMode,input BurstTyp
    
    
    
-   //Output Logic
+   //Output Logic  ~ Noteworthy - The SWITCH states are used rather than simply reverting back to the start state
+   // because this would cause the high part of the signal to be low for one clock cycle (since start sets PWM to 0)
   always @(SysClk) begin
     case(CurrentState) 
     START: begin
