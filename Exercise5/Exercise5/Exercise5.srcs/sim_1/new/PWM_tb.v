4ce7888793150aa87c9dd57b4e93f6bcbcd70e83
Dawson Chitwood <dchitwood33@gmail.com>
2025-02-13 10:15:00 -0500

    PWM testbench done

---
+++ Exercise5/Exercise5/Exercise5.srcs/sim_1/new/PWM_tb.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/13/2025 10:02:53 AM
+// Design Name: 
+// Module Name: PWM_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module PWM_tb();
+reg [7:0] duty;
+reg SysClk;
+reg Reset;
+wire pwm;
+parameter FIFTY = 128;
+parameter TWENTYFIVE = 64;
+parameter  SEVENTYFIVE = 192;
+
+PWM pwmtb(duty,SysClk,Reset,pwm);
+
+initial begin
+    SysClk = 0;
+    Reset = 0;
+    duty = 104;
+end
+
+always begin
+    #1 SysClk = 1;
+    #1 SysClk = 0;
+end
+
+
+
+endmodule
