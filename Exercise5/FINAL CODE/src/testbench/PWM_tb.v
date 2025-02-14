effd32f2cd9857eae50f8a658e0dbb90812696ba
Dawson Chitwood <dchitwood33@gmail.com>
2025-02-14 17:34:06 -0500

    added commented PWM_tb.v to final code file

--- Exercise5/FINAL CODE/src/testbench/PWM_tb.v
+++ Exercise5/FINAL CODE/src/testbench/PWM_tb.v
@@ -19,24 +19,32 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
+//PWM testbench
 module PWM_tb();
-reg [7:0] duty;
-reg SysClk;
-reg Reset;
-wire pwm;
-parameter FIFTY = 128;
+reg [7:0] duty;  //duty cycle input
+reg SysClk;     //clock signal
+reg Reset;      //reset signal
+wire pwm;      //pwm output signal
+
+//parameters for easy duty cycle encoding
+parameter FIFTY = 128;   
 parameter TWENTYFIVE = 64;
 parameter  SEVENTYFIVE = 192;
 
+
+//module instantiation
 PWM pwmtb(duty,SysClk,Reset,pwm);
 
+
+//initialization of variables
 initial begin
     SysClk = 0;
     Reset = 0;
-    duty = TWENTYFIVE;
+    duty = 0;
 end
 
+
+//always block for toggling the clock
 always begin
     #1 SysClk = 1;
     #1 SysClk = 0;
