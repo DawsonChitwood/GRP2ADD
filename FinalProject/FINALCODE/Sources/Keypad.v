1dce075e1135cd7753930fe8cb48fcd6e8c490fb
DawsonChitwood <122295606+DawsonChitwood@users.noreply.github.com>
2025-04-26 12:03:40 -0400

    Update Keypad.v

--- FinalProject/FINALCODE/Sources/Keypad.v
+++ FinalProject/FINALCODE/Sources/Keypad.v
@@ -20,6 +20,10 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
+
+//This module was not used in the final design, however its contents were. Our controller is merely an array of buttons that are all tied to a common 3v3 rail (current limited by a 1k ohm resistor).
+// Each time a button is pressed, its alternate pin will saturate to 3v3 volts, which means the PMOD pin it is connected to will also saturate to 3v3 volts. Now, for some reason our controller worked the 
+// opposite way when we implemented it. It acted as an active low, meaning each button actually pulled the high voltage of the PMOD pins down to low when pressed. 
 module Keypad(input CLK100MHZ,input [1:0]SW, input [5:0] JA,output [5:0] LED);
 
 assign LED[0] = JA[0];
