8f3e738e01e6a278ee2c48a9789fc1d0da64bb0f
DawsonChitwood <122295606+DawsonChitwood@users.noreply.github.com>
2025-04-26 11:24:50 -0400

    Update Auto_Clk.v

--- FinalProject/FINALCODE/Sources/Auto_Clk.v
+++ FinalProject/FINALCODE/Sources/Auto_Clk.v
@@ -19,10 +19,12 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+//This module divides the 100 MHZ clock into 50 MHZ
 
 module Auto_Clk(input Clk, output  Clk_50MHZ);
 reg Clk_50MHZ_reg = 0;
 
+// It does so by flipping the polarity of the 50MHZ clock on every positive edge of the 100 MHZ clock; effectively halfing the frequency.
 always @(posedge Clk) begin
     Clk_50MHZ_reg <= ~Clk_50MHZ_reg;
 end
